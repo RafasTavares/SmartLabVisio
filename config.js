01ecad905c953d40c9c5c503afb59677b8364249
Rafael Tavares <rafaeltavaresandrade@gmail.com>
2017-12-05 09:59:43 -0300

    Inserindo projeto

---
+++ config.js
@@ -0,0 +1,357 @@
+var CONFIG = {
+ "data": {
+  "lastModified": 1512477372177,
+  "path": "./data/temp in smartlab.csv",
+  "reader": "csv-time_in_columns",
+  "ddfPath": "./data/temp in smartlab.csv"
+ },
+ "locale": {
+  "filePath": "assets/translation/",
+  "id": "en"
+ },
+ "state": {
+  "entities": {
+   "autoconfig": {
+    "excludeIDs": [
+     "tag"
+    ],
+    "type": "entity_domain"
+   },
+   "dim": "Aluno",
+   "show": {},
+   "skipFilter": false
+  },
+  "entities_colorlegend": {
+   "autoconfig": {
+    "excludeIDs": [
+     "tag"
+    ],
+    "type": "entity_domain"
+   },
+   "dim": "Aluno",
+   "show": {},
+   "skipFilter": false
+  },
+  "entities_tags": {
+   "autoconfig": {
+    "includeOnlyIDs": [
+     "tag"
+    ],
+    "type": "entity_domain"
+   },
+   "dim": null,
+   "show": {},
+   "skipFilter": false
+  },
+  "marker": {
+   "allowSelectMultiple": true,
+   "axis_x": {
+    "allow": {
+     "scales": [
+      "linear",
+      "log",
+      "genericLog",
+      "time",
+      "pow"
+     ]
+    },
+    "autoconfig": {
+     "index": 0,
+     "type": "measure"
+    },
+    "data": "data",
+    "domainMax": null,
+    "domainMin": null,
+    "scaleType": "linear",
+    "use": "indicator",
+    "which": "Temperatuda do Lab.",
+    "zoomedMax": null,
+    "zoomedMin": null
+   },
+   "axis_y": {
+    "allow": {
+     "scales": [
+      "linear",
+      "log",
+      "genericLog",
+      "time",
+      "pow"
+     ]
+    },
+    "autoconfig": {
+     "index": 1,
+     "type": "measure"
+    },
+    "data": "data",
+    "domainMax": null,
+    "domainMin": null,
+    "scaleType": "linear",
+    "use": "indicator",
+    "which": "Luminosidade do Lab.",
+    "zoomedMax": null,
+    "zoomedMin": null
+   },
+   "color": {
+    "allow": {
+     "scales": [
+      "linear",
+      "log",
+      "genericLog",
+      "time",
+      "pow",
+      "ordinal"
+     ]
+    },
+    "autoconfig": {},
+    "data": "data",
+    "palette": {
+     "0": "#4cd843",
+     "1": "#e83739",
+     "2": "#ff7f00",
+     "3": "#c027d4",
+     "4": "#d66425",
+     "5": "#0ab8d8",
+     "6": "#bcfa83",
+     "7": "#ff8684",
+     "8": "#ffb04b",
+     "9": "#f599f5",
+     "10": "#f4f459",
+     "11": "#7fb5ed",
+     "_default": "#ffb600"
+    },
+    "paletteLabels": null,
+    "scaleType": "ordinal",
+    "syncModels": [
+     "marker_colorlegend"
+    ],
+    "use": "property",
+    "which": "Aluno"
+   },
+   "highlight": [],
+   "label": {
+    "autoconfig": {
+     "includeOnlyIDs": [
+      "name"
+     ],
+     "type": "string"
+    },
+    "data": "data",
+    "scaleType": "ordinal",
+    "use": "property",
+    "which": "Aluno"
+   },
+   "opacityHighlightDim": 0.1,
+   "opacityRegular": 1,
+   "opacitySelectDim": 0.3,
+   "select": [],
+   "size": {
+    "allow": {
+     "scales": [
+      "ordinal",
+      "linear",
+      "log",
+      "genericLog",
+      "pow"
+     ]
+    },
+    "autoconfig": {
+     "index": 2,
+     "type": "measure"
+    },
+    "data": "data",
+    "domainMax": null,
+    "domainMin": null,
+    "extent": [
+     0,
+     0.85
+    ],
+    "scaleType": "linear",
+    "use": "indicator",
+    "which": "Luminosidade do Lab.",
+    "zoomedMax": null,
+    "zoomedMin": null
+   },
+   "size_label": {
+    "_important": false,
+    "allow": {
+     "names": [
+      "_default"
+     ],
+     "scales": [
+      "ordinal",
+      "linear",
+      "log",
+      "genericLog",
+      "pow"
+     ]
+    },
+    "data": "data",
+    "domainMax": null,
+    "domainMin": null,
+    "extent": [
+     0,
+     0.33
+    ],
+    "scaleType": "ordinal",
+    "use": "constant",
+    "which": "_default",
+    "zoomedMax": null,
+    "zoomedMin": null
+   },
+   "skipFilter": false,
+   "space": [
+    "entities",
+    "time"
+   ],
+   "superHighlight": []
+  },
+  "marker_colorlegend": {
+   "allowSelectMultiple": true,
+   "highlight": [],
+   "hook_geoshape": {
+    "data": "data",
+    "use": "property",
+    "which": "shape_lores_svg"
+   },
+   "hook_rank": {
+    "data": "data",
+    "use": "property",
+    "which": "rank"
+   },
+   "label": {
+    "data": "data",
+    "use": "property",
+    "which": "name"
+   },
+   "opacityHighlightDim": 0.1,
+   "opacityRegular": 1,
+   "opacitySelectDim": 0.3,
+   "select": [],
+   "skipFilter": false,
+   "space": [
+    "entities_colorlegend"
+   ],
+   "superHighlight": []
+  },
+  "marker_tags": {
+   "allowSelectMultiple": true,
+   "highlight": [],
+   "hook_parent": {
+    "data": "data",
+    "which": null
+   },
+   "label": {
+    "data": "data",
+    "use": "property",
+    "which": "name"
+   },
+   "opacityHighlightDim": 0.1,
+   "opacityRegular": 1,
+   "opacitySelectDim": 0.3,
+   "select": [],
+   "skipFilter": false,
+   "space": [
+    "entities_tags"
+   ],
+   "superHighlight": []
+  },
+  "time": {
+   "autoconfig": {
+    "type": "time"
+   },
+   "delay": 150,
+   "delayThresholdX2": 90,
+   "delayThresholdX4": 45,
+   "dim": "time",
+   "end": "20171130",
+   "endOrigin": null,
+   "endSelected": "20171130",
+   "format": {
+    "data": null,
+    "ui": null
+   },
+   "immediatePlay": true,
+   "loop": false,
+   "offset": 0,
+   "playable": true,
+   "playing": false,
+   "record": false,
+   "round": "round",
+   "start": "20171101",
+   "startOrigin": null,
+   "startSelected": "20171101",
+   "step": 1,
+   "unit": "day",
+   "value": "20171130"
+  }
+ },
+ "ui": {
+  "adaptMinMaxZoom": false,
+  "buttons": [
+   "colors",
+   "find",
+   "zoom",
+   "trails",
+   "lock",
+   "moreoptions",
+   "fullscreen",
+   "presentation"
+  ],
+  "chart": {
+   "labels": {
+    "dragging": true,
+    "removeLabelBox": false
+   },
+   "lockNonSelected": 0,
+   "margin": {
+    "left": 89.442,
+    "top": 0
+   },
+   "superhighlightOnMinimapHover": true,
+   "trails": true,
+   "whenHovering": {
+    "higlightValueX": true,
+    "higlightValueY": true,
+    "showProjectionLineX": true,
+    "showProjectionLineY": true
+   }
+  },
+  "cursorMode": "arrow",
+  "datawarning": {
+   "doubtDomain": [],
+   "doubtRange": []
+  },
+  "dialogs": {
+   "moreoptions": [
+    "opacity",
+    "speed",
+    "axes",
+    "size",
+    "colors",
+    "label",
+    "zoom",
+    "presentation",
+    "about"
+   ],
+   "popup": [
+    "colors",
+    "find",
+    "size",
+    "zoom",
+    "moreoptions"
+   ],
+   "sidebar": [
+    "colors",
+    "find",
+    "size",
+    "zoom"
+   ]
+  },
+  "panWithArrow": false,
+  "presentation": false,
+  "show_ticks": true,
+  "splash": false,
+  "zoomOnScrolling": false
+ },
+ "chartType": "BubbleChart"
+};
